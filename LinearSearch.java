a1656abff415590b3a184f6a29c8809cddfbdc94
Lina Nhayade <lbnhayade08@gmail.com>
2026-05-06 17:10:41 +0530

    added linear search

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,6 +1,37 @@
+import java.util.*;
 
-public class LinearSearch{
-    public static void main(String[] args) {     
+public class LinearSearch {
 
+    static void linearsearch(int arr[], int target) {
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Element not found.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the " + (i + 1) + " element: ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Array: [");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("]");
+        System.out.print("Enter the element to be searched: ");
+        int target = sc.nextInt();
+        linearsearch(arr, target);
     }
 }
